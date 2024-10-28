7d70501d5b6918bd84400b7561ecfb34d66547de
prakriti16 <70594062+prakriti16@users.noreply.github.com>
2024-10-28 19:37:23 +0530

    Update server.js

--- src/backend_folder/server.js
+++ src/backend_folder/server.js
@@ -119,6 +119,6 @@ app.get('/', (req, res) => {
   res.send("Welcome to the API");
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
