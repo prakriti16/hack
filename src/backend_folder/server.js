b79a859fff7677bf94cc9edab9722727e435a58b
prakriti16 <70594062+prakriti16@users.noreply.github.com>
2024-10-28 20:02:35 +0530

    Update server.js

--- src/backend_folder/server.js
+++ src/backend_folder/server.js
@@ -120,5 +120,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${PORT}`);
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
